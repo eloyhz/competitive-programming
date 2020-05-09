a966a572dcf709c3c47bf61652b807c090d9031d
Eloy Hernandez <eloyhz@gmail.com>
2020-05-09 11:17:39 -0500

    D. Alice, Bob and Candies [AC]

---
+++ codeforces/round-640-div4/d.py
@@ -0,0 +1,35 @@
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = [int(x) for x in input().split()]
+        alice = []
+        bob = []
+        move = 1
+        alice.append(a[0])
+        last = a[0]
+        a.pop(0)
+        turn = "b"
+        while len(a) > 0:
+            if turn == "a":
+                s = 0
+                while last >= 0 and len(a) > 0:
+                    candie = a[0]
+                    a.pop(0)
+                    last -= candie
+                    s += candie
+                    alice.append(candie)
+                turn = "b"
+                last = s
+            elif turn == "b":
+                s = 0
+                while last >= 0 and len(a) > 0:
+                    candie = a[-1]
+                    a.pop()
+                    last -= candie
+                    s += candie
+                    bob.append(candie)
+                last = s
+                turn = "a"
+            move += 1
+        print(move, sum(alice), sum(bob))
