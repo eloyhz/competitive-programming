5a610eec1366c5f99de37fc0ff27dddd0e9a5d5f
Eloy Hernandez <eloyhz@gmail.com>
2020-05-14 11:24:28 -0500

    Codeforces - A. Most Unstable Array [AC]

--- codeforces/round-642-div3/a.py
+++ codeforces/round-642-div3/a.py
@@ -3,8 +3,14 @@
 
 
 if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        n, m = [int(x) for x in input().split()]
-        # n = length of the array
-        # m = sum of the array
+	t = int(input())
+	for _ in range(t):
+		n, m = [int(x) for x in input().split()]
+		if n == 1:
+			print(0)
+			continue
+		if n == 2:
+			print(m)
+			continue
+		print(m * 2)
+
