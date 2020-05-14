657fdc66a4edb2fd49b2aa82155a72b36bfe7638
Eloy Hernandez <eloyhz@gmail.com>
2020-05-14 11:36:28 -0500

    Codeforces - Round 642 - C. Board Moves [AC] in the last 20 seconds

--- codeforces/round-642-div3/c.py
+++ codeforces/round-642-div3/c.py
@@ -6,18 +6,7 @@ if __name__ == "__main__":
     t = int(input())
     for _ in range(t):
         n = int(input())
-        board = [[0 for _ in range(n)] for _ in range(n)]
-        k = n // 2
-        for i in range(n // 2):
-            for j in range(i, n - i):
-                # fill rows
-                board[i][j] = k
-                board[n - i - 1][j] = k
-                # fill columns
-                board[j][i] = k
-                board[j][n - i - 1] = k
-            k -= 1
         result = 0
-        for b in board:
-            result += sum(b)
+        for i in range(1, n // 2 + 1):
+            result += 8 * i * i
         print(result)
