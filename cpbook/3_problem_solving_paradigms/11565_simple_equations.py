f8258ba7859a1f500e504baea99073d424aa4499
Eloy Hernandez <eloyhz@gmail.com>
2020-05-15 14:44:33 -0500

    UVa 11565 - Simple Equations [AC in Python]

--- cpbook/3_problem_solving_paradigms/11565_simple_equations.py
+++ cpbook/3_problem_solving_paradigms/11565_simple_equations.py
@@ -2,10 +2,34 @@
 # https://onlinejudge.org/external/115/11565.pdf
 
 
-def solve():
+def solve_tle():
 	a, b, c = [int(i) for i in input().split()]
 	for x in range(-100, 101):
 		for y in range(-100, 101):
+			for z in range(-100, 101):
+				if x == y or x == z or y == z:
+					continue
+				if x + y + z != a:
+					continue
+				if x * y * z != b:
+					continue
+				if x * x + y * y + z * z != c:
+					continue
+				print(x, y, z)
+				return True
+	return False
+
+
+def solve():
+	a, b, c = [int(i) for i in input().split()]
+	for x in range(-22, 23):
+		if x * x > c:
+			continue
+		for y in range(-100, 101):
+			if y == x:
+				continue
+			if x * x + y * y > c:
+				continue
 			for z in range(-100, 101):
 				if x == y or x == z or y == z:
 					continue
