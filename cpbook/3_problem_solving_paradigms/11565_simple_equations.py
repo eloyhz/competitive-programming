5a299b47ed1c2ea5d61939b270d37ee1c179a433
Eloy Hernandez <eloyhz@gmail.com>
2020-05-15 14:39:09 -0500

    UVa 11565 - Simple Equations [TLE in Python]

---
+++ cpbook/3_problem_solving_paradigms/11565_simple_equations.py
@@ -0,0 +1,27 @@
+# UVa 11565 - Simple Equations
+# https://onlinejudge.org/external/115/11565.pdf
+
+
+def solve():
+	a, b, c = [int(i) for i in input().split()]
+	for x in range(-100, 101):
+		for y in range(-100, 101):
+			for z in range(-100, 101):
+				if x == y or x == z or y == z:
+					continue
+				if x + y + z != a:
+					continue
+				if x * y * z != b:
+					continue
+				if x * x + y * y + z * z != c:
+					continue
+				print(x, y, z)
+				return True
+	return False
+
+
+if __name__ == "__main__":
+	t = int(input())
+	for _ in range(t):
+		if not solve():
+			print("No solution.")
