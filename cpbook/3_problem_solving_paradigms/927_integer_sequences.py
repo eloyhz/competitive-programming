176d75e1946d6a21ab271b29df301e01bb53d58e
Eloy Hernandez <eloyhz@gmail.com>
2020-05-18 14:27:50 -0500

    UVa 00927 - Integer Sequences [AC] Refactoring

--- cpbook/3_problem_solving_paradigms/927_integer_sequences.py
+++ cpbook/3_problem_solving_paradigms/927_integer_sequences.py
@@ -2,21 +2,31 @@
 # https://onlinejudge.org/external/9/927.pdf
 
 
-def test_case():
-	a = [int(x) for x in input().split()]
-	deg = a[0]
-	a.pop(0)
-	d = int(input())
-	k = int(input())
+def find_n(d, k):
 	s = 0
 	n = 0
 	while s < k:
 		n += 1
 		s += n * d
+	return n
+
+
+def eval_poly(a, n):
 	ev = 0
-	for i in range(deg + 1):
+	deg = len(a)
+	for i in range(deg):
 		ev += a[i] * n ** i
-	print(ev)
+	return ev
+
+
+def test_case():
+	a = [int(x) for x in input().split()]
+	a.pop(0)
+	d = int(input())
+	k = int(input())
+	n = find_n(d, k)
+	result = eval_poly(a, n)
+	print(result)
 
 
 if __name__ == "__main__":
