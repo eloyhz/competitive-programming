3ebce8f5ff4942eb71c6f9b93fc2053ca0051985
Eloy Hernandez <eloyhz@gmail.com>
2020-05-22 19:55:31 -0500

    UVa 732 - Anagrams by Stack [AC][CPP] at last

--- cpbook/2_data_structures/732_anagrams_by_stack.cpp
+++ cpbook/2_data_structures/732_anagrams_by_stack.cpp
@@ -2,73 +2,15 @@
 // https://onlinejudge.org/external/7/732.pdf
 
 #include <iostream>
-#include <bitset>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <stack>
-#include <queue>
-#include <chrono>
 
 using namespace std;
 
 string source, target;
 int n;
-bitset<32> solution;
-vector<string> solutions;
-vector<string> result;
-
-bool is_valid(const string& sol)	
-{
-	stack<char> st;
-	queue<char> qt;
-	string rt = "";
-
-	for (auto c : source)	{
-		qt.push(c);
-	}
-	for (auto c : sol)	{
-		if (c == 'i')	{		// push
-			if (qt.empty())	{
-				return false;
-			}
-			st.push(qt.front());
-			qt.pop();
-		}
-		else if (c == 'o') 	{			//	pop
-			if (st.empty())	{
-				return false;
-			}
-			rt += st.top();
-			st.pop();
-		}
-	}
-	if (rt == target)	{
-		return true;
-	}	else	{
-		return false;
-	}
-}
-
-void generate(int k, int i, int o)	
-{
-	if (o > i || i > n)	{
-		return;
-	}
-	if (k == n * 2)	{
-		string sol = "";
-		for (int i = 0; i < k; i++)	{
-			sol += (solution.test(i) ? "i" : "o");
-			sol += (i < k - 1 ? " " : "");
-		}
-		solutions.push_back(sol);
-	}	else	{
-		solution.set(k);
-		generate(k + 1, i + 1, o);
-		solution.reset(k);
-		generate(k + 1, i, o + 1);
-	}
-}
 
 bool are_anagrams()
 {
@@ -83,27 +25,6 @@ bool are_anagrams()
 		return false;
 	}
 	return true;
-}
-
-void print_result()
-{
-	cout << "[\n";
-	for (auto r : result)	{
-		cout << r << "\n";
-	}
-	cout << "]\n";
-}
-
-void get_result()
-{
-	result.clear();
-	for (auto s : solutions)	{
-		if (is_valid(s))	{
-			result.push_back(s);
-		}
-	}
-	sort(result.begin(), result.end());
-	print_result();
 }
 
 void build_path(vector<char> path, stack<char> st, char next_op, int ci, int co, int i, int o)
@@ -127,14 +48,16 @@ void build_path(vector<char> path, stack<char> st, char next_op, int ci, int co,
 		st.pop();
 	}
 	if (path.size() == n * 2)	{
-		for (auto p : path)	{
-			cout << p;
+		for (int i = 0; i < path.size(); i++)	{
+			cout << path[i];
+			cout << (i < path.size() - 1 ? " " : "");
 		}
-		cout << endl;
+		cout << "\n";
 	}	else	{
 		if (i < n)	{
 			build_path(path, st, 'i', ci, co, i, o);
-		}	else	{
+		}
+		if (o < n)  {
 			build_path(path, st, 'o', ci, co, i, o);
 		}
 	}
@@ -146,16 +69,12 @@ void test_case()
 		cout << "[\n]\n";
 		return;
 	}
+    n = source.size();
 	vector<char> p;
 	stack<char> s;
+    cout << "[\n";
 	build_path(p, s, 'i', 0, 0, 0, 0);
-	/*
-	n = source.size();
-	solution.reset();
-	solution.set(0);
-	generate(1, 1, 0);
-	get_result();
-	*/
+    cout << "]\n";
 }
 
 int main()
