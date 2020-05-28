a9700dc29859dae53a81f32b94e7fc32ec644bf3
Eloy Hernandez <eloyhz@gmail.com>
2020-05-27 22:30:08 -0500

    UVa 11553 - Grid Game [AC][PY]

--- cpbook/3_problem_solving_paradigms/11553_grid_game.py
+++ cpbook/3_problem_solving_paradigms/11553_grid_game.py
@@ -3,47 +3,26 @@
 
 from itertools import permutations
 from sys import maxsize
-import copy
-
-memo_perm = {}
 
 def game(n, grid, order):
 	alice_candies = 0
-	for i in order:
-		bob_column = 0
-		for j in range(1, n):
-			if grid[i][j] < grid[i][bob_column]:
-				bob_column = j
-		alice_candies += grid[i][bob_column]
-		for k in range(n):
-			grid[k][bob_column] = 1001
+	for i in range(n):
+		alice_candies += grid[i][order[i]]
 	return alice_candies
-		
+	
 
 def test_case():
-	global memo_perm
 	n = int(input())
 	grid = []
 	for _ in range(n):
 		row = [int(x) for x in input().split()]
 		grid.append(row)
-	if n in memo_perm:
-		perm = memo_perm[n]
-	else:
-		memo_perm[n] = perm = list(permutations(range(n)))
-	best_result_neg = 0
-	best_result_pos = 0
-	# print("Case")
+	perm = list(permutations(range(n)))
+	best = maxsize
 	for p in perm:
-		g = copy.deepcopy(grid)
-		result = game(n, g, p)
-		# print(f"{p}: {result}")
-		# best_result = max(best_result, result)
-		if result < 0:
-			best_result_neg = min(best_result_neg, result)
-		else:
-			best_result_pos = max(best_result_pos, result)
-	print(best_result_neg if best_result_neg < 0 else best_result_pos)
+		result = game(n, grid, p)
+		best = min(best, result)
+	print(best)
 
 
 if __name__ == "__main__":
