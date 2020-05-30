ebf4adf0e0c2df4831df290fe6bb1d880963c64e
Eloy Hernandez <eloyhz@gmail.com>
2020-05-29 23:13:02 -0500

    UVa 729 - The Hamming Distance Problem [AC] Using backtracking (48729_the_hamming_distance_problem.py faster)

--- cpbook/3_problem_solving_paradigms/729_the_hamming_distance_problem.py
+++ cpbook/3_problem_solving_paradigms/729_the_hamming_distance_problem.py
@@ -1,7 +1,27 @@
 # UVa 729 - The Hamming Distance Problem
 # https://onlinejudge.org/external/7/729.pdf
 
+bits = []
 
+def backtrack(n, h, k):
+	if len(bits) == n:
+		if bits.count(1) == h:
+			print(''.join([str(b) for b in bits]))
+		return
+	bits.append(0)
+	backtrack(n, h, k + 1)
+	bits.pop()
+	bits.append(1)
+	backtrack(n, h, k + 1)
+	bits.pop()
+
+
+# Using backtracking: 1.350 secs (48% faster)
+def test_case_backtrack(n, h):
+	backtrack(n, h, 0)
+
+
+# Using bit manipulation: 2.790 secs
 def test_case(n, h):
 	for i in range(1 << n):
 		bits = 0
@@ -13,12 +33,13 @@ def test_case(n, h):
 			print(format(i, '0' + str(n) + 'b'))
 
 
+
 if __name__ == "__main__":
 	tc = int(input())
 	input()
 	for t in range(tc, 0, -1):
 		n, h = [int(x) for x in input().split()]
-		test_case(n, h)
+		test_case_backtrack(n, h)
 		if t > 1:
 			input()
 			print()
