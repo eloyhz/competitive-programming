35f3a469aa0adf408fbc370ec122712e26c36abd
Eloy Hernandez <eloyhz@gmail.com>
2020-05-30 12:48:39 -0500

    Exercism - Hamming Distance using Python zip

--- exercism/python/hamming/hamming.py
+++ exercism/python/hamming/hamming.py
@@ -1,10 +1,9 @@
 def distance(strand_a, strand_b):
 	if len(strand_a) != len(strand_b):
 		raise ValueError("Hamming distance is only defined for sequences of equal length")
-	differences = 0
-	n = len(strand_a)
-	for i in range(n):
-		if strand_a[i] != strand_b[i]:
-			differences += 1
-	return differences
-
+	return sum([1 if s[0] != s[1] else 0 for s in zip(strand_a, strand_b)])
+	# n = len(strand_a)
+	# for i in range(n):
+	#	if strand_a[i] != strand_b[i]:
+	#		differences += 1
+	# return differences
