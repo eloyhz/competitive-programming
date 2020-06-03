cf42943e7acfe55fdf7a217e7811bf24fab1da6a
Eloy Hernandez <eloyhz@gmail.com>
2020-06-03 14:49:41 -0500

    UVa 12192 - Grapevine [V2][TLE]

--- cpbook/3_problem_solving_paradigms/12192_grapevine.py
+++ cpbook/3_problem_solving_paradigms/12192_grapevine.py
@@ -4,68 +4,115 @@
 import bisect
 import time
 
-# Similar to C++'s lower_bound
-# Finally use Python's bisect
-def bin_search_lower(arr, key):
-	n = len(arr)
-	lo = 0
-	hi = n - 1
-	while hi - lo > 1:
-		mid = lo + (hi - lo) // 2
-#		print(lo, mid, hi)
-		if key < arr[mid]:
-			hi = mid
-		else:
-			lo = mid
-	return hi + 1 if arr[hi] == key else lo + 1
+def fill_diagonals(n, m, field):
+	diagonals = []
+	if n >= m:
+		diff = n - m
+		size = m
+		for i in range(m):
+			diag = []
+			for s in range(size):
+				diag.append(field[0 + s][i + s])
+			size -= 1
+			diagonals.append(diag)
+		row = 1
+		size = m
+		for _ in range(diff):
+			diag = []
+			for s in range(size):
+				diag.append(field[row + s][0 + s])
+			diagonals.append(diag)
+			row += 1
+		size = m - 1
+		for _ in range(m - 1):
+			diag = []
+			for s in range(size):
+				diag.append(field[row + s][0 + s])
+			diagonals.append(diag)
+			row += 1
+			size -= 1
+	else:
+		diff = m - n
+		size = n
+		for i in range(n):
+			diag = []
+			for s in range(size):
+				diag.append(field[i + s][0 + s])
+			diagonals.append(diag)
+			size -= 1
+		col = 1
+		size = n
+		for _ in range(diff):
+			diag = []
+			for s in range(size):
+				diag.append(field[0 + s][col + s])
+			diagonals.append(diag)
+			col += 1
+		size = n - 1
+		for  _ in range(n - 1):
+			diag = []
+			for s in range(size):
+				diag.append(field[0 + s][col + s])
+			diagonals.append(diag)
+			col += 1
+			size -= 1
+	return diagonals
 
-def best_lower(n, m, field, l):
-	lower = []
-	for i, f in enumerate(field):
-		low = bisect.bisect(f, l)
-		prev = False
-		while 0 < low < m and f[low - 1] == l:
-			prev = True
-			low -= 1
-			lower.append((min(m - low if m > low else 1, n - i), i, low))
-		if not prev:
-			lower.append((min(m - low if m > low else 1, n - i), i, low))
-	return sorted(lower, reverse=True)
 
-
-def best_upper(n, m, field, lower, u):
+def find_best(diagonals, lower, upper):
 	best = 0
-	for l in lower:
-		side = l[0]
-		while side >= 1:
-			i = l[1] + side - 1
-			j = l[2] + side - 1
-			if i < 0 or i >= n or j < 0 or j >= m:
-				break
-			if field[i][j] <= u:
-				best = max(best, side)
-				break
-			side -= 1
+	for diag in diagonals:
+		l = bisect.bisect_left(diag, lower)
+		u = bisect.bisect_right(diag, upper)
+		"""
+		if upper == diag[-1]:
+			u = bisect.bisect(diag, upper)
+		else:
+			u = bisect.bisect_left(diag, upper)
+		"""
+		# print(f"diag={diag}, lower={lower}, l={l}, upper={upper}, u={u}, u-l={u - l}")
+		if u == l and u < len(diag) and lower <= diag[u] <= upper:
+			best = max(best, 1)
+		else:
+			best = max(best, u - l)
 	return best
-
 
 def test_case(n, m):
 	field = []
 	for _ in range(n):
 		field.append([int(x) for x in input().split()])
+	diagonals = fill_diagonals(n, m, field)
 	q = int(input())
 	for _ in range(q):
 		l, u = [int(x) for x in input().split()]
-		lower = best_lower(n, m, field, l)
-		upper = best_upper(n, m, field, lower, u)
-		print(upper)
+		best = find_best(diagonals, l, u)
+		print(best)
 	print("-")
 		
 
 def test():
-	n = 50
-	print(bin_search_lower([13, 21, 25, 33, 34], n))
-	print(bisect.bisect([13, 21, 25, 33, 34], n))
+	a = [[1, 2, 3, 4],\
+			 [5, 6, 7, 8], \
+			 [9, 10, 11, 12], \
+			 [13, 14, 15, 16], \
+			 [17, 18, 19, 20], \
+			 [21, 22, 23, 24]]
+	da = fill_diagonals(6, 4, a)
+	for d in da:
+		print(d)
+	b = []
+	count = 1
+	for _ in range(4):
+		d = []
+		for _ in range(6):
+			d.append(count)
+			count += 1
+		b.append(d)
+	print(b)
+	db = fill_diagonals(4, 6, b)
+	for d in db:
+		print(d)
+
 
 if __name__ == "__main__":
 	# start_time = time.time()
