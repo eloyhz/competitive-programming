8a4ceb7a8f7d285d7d615538ad211f1fa77eb6e8
Eloy Hernandez <eloyhz@gmail.com>
2020-06-15 22:49:01 -0500

    UVa 507 - Jill Rides Again [WA]

---
+++ cpbook/3_problem_solving_paradigms/507_jill_rides_again.py
@@ -0,0 +1,35 @@
+# UVa 507 - Jill Rides Again
+# https://onlinejudge.org/external/5/507.pdf
+
+def niceness_route(stops):
+	s = a = 0
+	n = len(stops)
+	start = end = 0
+	best_start = best_end = 0
+	for i in range(n):
+		s += stops[i]
+		if s >= a:
+			a = s
+			end = i
+			if end - start > best_end - best_start:
+				best_start = start
+				best_end = end
+		if s < 0:
+			s = 0
+			start = i
+	# print(s, a, stops, start + 2, end + 2)
+	return (-1, -1) if a == 0 else (best_start + 2, best_end + 2)
+
+if __name__ == "__main__":
+	routes = int(input())
+	for r in range(routes):
+		s = int(input())
+		stops = []
+		for _ in range(s - 1):
+			stops.append(int(input()))
+		i, j = niceness_route(stops)
+		if i == j == -1:
+			print(f"Route {r + 1} has no nice parts")
+		else:
+			print(f"The nicest part of route {r + 1} is between stops {i} and {j}")
+
