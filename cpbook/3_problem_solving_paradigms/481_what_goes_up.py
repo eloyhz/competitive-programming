29b2b4c5b99a080f43eab60ef0768047d7a6bfd6
Eloy Hernandez <eloyhz@gmail.com>
2020-06-19 23:04:48 -0500

    UVa 481 - What Goes Up [AC][PY]

--- cpbook/3_problem_solving_paradigms/481_what_goes_up.py
+++ cpbook/3_problem_solving_paradigms/481_what_goes_up.py
@@ -2,9 +2,10 @@
 # https://onlinejudge.org/external/4/481.pdf
 
 from sys import stdin
+from bisect import bisect_left
 
 
-def lis(sequence):
+def lis_tle(sequence):
 	n = len(sequence)
 	prev = [0] * n
 	length = [0] * n
@@ -29,6 +30,32 @@ def lis(sequence):
 	while len(stack) > 0:
 		print(stack.pop())
 
+
+def lis(array):
+	n = len(array)
+	L = [array[0]]
+	indices = [0]
+	prev = [None] * n
+	for i in range(1, n):
+		num = array[i]
+		j = bisect_left(L, num)
+		if j < len(L):
+			L[j] = num
+			indices[j] = i
+		elif num != L[-1]:
+			L.append(num)
+			indices.append(i)
+		if j > 0:
+			prev[i] = indices[j - 1]
+	stack = []
+	p = indices[-1]
+	while p != None:
+		stack.append(array[p])
+		p = prev[p]
+	print(len(stack))
+	print("-")
+	while len(stack) > 0:
+		print(stack.pop())
 
 
 if __name__ == "__main__":
