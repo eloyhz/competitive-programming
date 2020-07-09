8a0d9f0ad1555a608aac4fb6de0a435b73c7bf9c
Eloy Hernandez <eloyhz@gmail.com>
2020-07-09 13:42:22 -0500

    TC-ARG-Warmup - Watermelon [AC]

---
+++ tc-arg/warmup/a_watermelon.py
@@ -0,0 +1,10 @@
+# Watermelon
+# https://codeforces.com/group/j1UosVRZar/contest/287018/problem/A
+
+if __name__ == "__main__":
+	n = int(input())
+	if n > 2 and n % 2 == 0:
+		print("YES")
+	else:
+		print("NO")
+
