c5376a25ace0d54b1f8242d5a0651ada076f1729
Eloy Hernandez <eloyhz@gmail.com>
2020-07-14 16:46:54 -0500

    TC-ARG-Contest 02 - Vasya and Chess [AC]

---
+++ tc-arg/contest-02/l_vasya_and_chess.py
@@ -0,0 +1,12 @@
+# TCArg 2020 - Contest #2
+# Vasya and Chess
+# https://codeforces.com/group/j1UosVRZar/contest/287504/problem/L
+
+if __name__ == "__main__":
+	n = int(input())
+	if n % 2 == 0:
+		print("white")
+		print("1 2")
+	else:
+		print("black")
+
