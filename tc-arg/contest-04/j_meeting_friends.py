6150eccef05d74703b90f31894f901021a956bfb
Eloy Hernandez <eloyhz@gmail.com>
2020-07-17 14:09:00 -0500

    TC-ARG-Contest 04 - Meeting Friends [WAx2]

--- tc-arg/contest-04/j_meeting_friends.py
+++ tc-arg/contest-04/j_meeting_friends.py
@@ -4,6 +4,6 @@
 
 if __name__ == "__main__":
 	x1, x2, x3 = [int(x) for x in input().split()]
-	midpoint = (x1 + x2 + x3) // 3
+	midpoint = round((x1 + x2 + x3) / 3)
 	min_distance = abs(x1 - midpoint) + abs(x2 - midpoint) + abs(x3 - midpoint)
 	print(min_distance)
