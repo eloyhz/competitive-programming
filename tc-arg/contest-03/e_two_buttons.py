45d9e1a8e0823a56df401de4f59521ac1cb133b2
Eloy <eloyhz@gmail.com>
2020-07-16 21:02:39 -0500

    TC-ARG-Contest 03 - Two Buttons [AC]

--- tc-arg/contest-03/e_two_buttons.py
+++ tc-arg/contest-03/e_two_buttons.py
@@ -2,8 +2,12 @@
 # Two Buttons
 # https://codeforces.com/group/j1UosVRZar/contest/287739/problem/E
 
-def solve(n, m):
-	counter = 0
+def solve_wa(n, m):
+	if m % 2 == 0:
+		counter = 0
+	else:
+		counter = 1
+		m += 1
 	half = m // 2
 	while n < half:
 		n *= 2
@@ -13,14 +17,22 @@ def solve(n, m):
 	else:
 		return counter + n - half + 1
 
+def solve(n, m):
+	counter = 0
+	while n < m:
+		if m % 2 == 0:
+			m //= 2
+		else:
+			m += 1
+		counter += 1
+	counter += n - m
+	return counter
+
 if __name__ == "__main__":
 	n, m = [int(x) for x in input().split()]
 	if n > m:
 		print(n - m)
 	elif n == m:
 		print(0)
-	elif m % 2 == 0:
-		print(solve(n, m))
 	else:
-		print(1 + solve(n, m + 1))
-
+		print(solve(n, m))
