ac635df09bb1523fdd884fb2bff2b137b322e458
Eloy Hernandez <eloyhz@gmail.com>
2020-07-21 14:49:25 -0500

    TC-ARG-Contest 06 - Digit sum [AC]

--- tc-arg/contest-06/d_digit_sum.py
+++ tc-arg/contest-06/d_digit_sum.py
@@ -1,16 +1,24 @@
 # TCArg 2020 - Contest #6
-# Digit sum [RTE]
+# Digit sum [AC]
 # https://codeforces.com/group/j1UosVRZar/contest/287776/problem/D
+
+def add_digits(num):
+	if num < 10:
+		return num
+	total = 0
+	while num >= 10:
+		total = 0
+		while num:
+			total += num % 10
+			num //= 10
+		num = total
+	return total
 
 if __name__ == "__main__":
 	a, b = [int(x) for x in input().split()]
 	num = a
+	result = add_digits(num)
 	for _ in range(1, b):
-		num *= a
-		while num >= 10:
-			total = 0
-			while num:
-				total += num % 10
-				num //= 10
-			num = total
-	print(total)
+		result *= a
+		result = add_digits(result)
+	print(result)
