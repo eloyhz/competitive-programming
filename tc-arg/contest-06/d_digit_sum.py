3eb027b2d8161822ae191d2f712b5554153bdf47
Eloy Hernandez <eloyhz@gmail.com>
2020-07-21 14:09:47 -0500

    TC-ARG-Contest 06 - Digit sum [RTE][PY]

--- tc-arg/contest-06/d_digit_sum.py
+++ tc-arg/contest-06/d_digit_sum.py
@@ -1,14 +1,16 @@
 # TCArg 2020 - Contest #6
-# Digit sum [TLE]
+# Digit sum [RTE]
 # https://codeforces.com/group/j1UosVRZar/contest/287776/problem/D
 
 if __name__ == "__main__":
 	a, b = [int(x) for x in input().split()]
-	num = a ** b
-	while num >= 10:
-		total = 0
-		while num:
-			total += num % 10
-			num //= 10
-		num = total
+	num = a
+	for _ in range(1, b):
+		num *= a
+		while num >= 10:
+			total = 0
+			while num:
+				total += num % 10
+				num //= 10
+			num = total
 	print(total)
