f5047c213d21e14eb4879f6b457ee9a8a61a28e1
Eloy Hernandez <eloyhz@gmail.com>
2020-07-27 23:10:56 -0500

    Kattis introductory problems

---
+++ cpbook/1_introduction/r2.py
@@ -0,0 +1,6 @@
+# Kattis - R2
+# https://open.kattis.com/problems/r2
+
+if __name__ == "__main__":
+	r1, s = [int(x) for x in input().split()]
+	print(2 * s - r1)
---
+++ cpbook/1_introduction/timeloop.py
@@ -0,0 +1,7 @@
+# Kattis - Stuck In A Time Loop
+# https://open.kattis.com/problems/timeloop
+
+if __name__ == "__main__":
+	n = int(input())
+	for i in range(n):
+		print(i + 1, "Abracadabra")
