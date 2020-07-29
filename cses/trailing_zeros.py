56e269567ff3503e5c8b9cfeae045eb8c170141d
Eloy Hernandez <eloyhz@gmail.com>
2020-07-29 13:06:58 -0500

    CSES - Trailing Zeros [AC]

--- cses/trailing_zeros.py
+++ cses/trailing_zeros.py
@@ -1,6 +1,12 @@
 # CSES - Trailing Zeros
 # https://cses.fi/problemset/task/1618
+# Ref: https://www.geeksforgeeks.org/count-trailing-zeroes-factorial-number/
 
 if __name__ == "__main__":
 	n = int(input())
-	print(n // 5)
+	count = 0
+	factor = 5
+	while factor <= n:
+		count += n // factor 
+		factor *= 5
+	print(count)
