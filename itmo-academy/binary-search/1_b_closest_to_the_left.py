759b5e63777ef14390c2ba6b9d4b8851e98a4a99
Eloy Hernandez <eloyhz@gmail.com>
2020-08-21 12:02:12 -0500

    ITMO Academy - Closest to the Left [AC]

---
+++ itmo-academy/binary-search/1_b_closest_to_the_left.py
@@ -0,0 +1,21 @@
+# ITMO Academy - Binary Search
+# 1.B. Closest to the Left
+# https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/B
+
+if __name__ == '__main__':
+	n, k = [int(x) for x in input().split()]
+	a = [int(x) for x in input().split()]
+	q = [int(x) for x in input().split()]
+
+	for x in q:
+		l = -1	# a[l] <= x
+		r = n	# a[r] > x
+		while l + 1 < r:
+			m = (l + r) // 2
+			if a[m] <= x:
+				l = m
+			else:
+				r = m
+		print(l + 1)
+
+
