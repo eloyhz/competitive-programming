8de0feee794949ee3b6181a4098ef82bbec30514
Eloy Hernandez <eloyhz@gmail.com>
2020-08-27 20:21:10 -0500

    ITMO Academy - Binary Search for Answer - Very Easy Task [AC]

--- itmo-academy/binary-search/2_c_very_easy_task.py
+++ itmo-academy/binary-search/2_c_very_easy_task.py
@@ -3,12 +3,15 @@
 # https://codeforces.com/edu/course/2/lesson/6/2/practice/contest/283932/problem/C
 
 def is_good(t, n, x, y):
-    return (t // x + (t - x) // y ) >= n
+    if x < y:
+        return (t // x + (t - x) // y ) >= n
+    else:
+        return ((t - y) // x + t // y ) >= n
     
 if __name__ == '__main__':
     n, x, y = [int(x) for x in input().split()]
     l = 0       # l is bad
-    r = 10 ** 9 # r is good
+    r = 10 ** 10  # r is good
     while r > l + 1:
         m = (l + r) // 2
         if is_good(m, n, x, y):
