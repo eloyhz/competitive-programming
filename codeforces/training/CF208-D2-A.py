71dd169fc1d6766e6b096f612e2cade03cdbf509
Eloy Hernandez <eloyhz@gmail.com>
2020-09-01 12:09:42 -0500

    Codeforces - Dubstep [AC]

---
+++ codeforces/training/CF208-D2-A.py
@@ -0,0 +1,22 @@
+# Codeforces - Dubstep
+# https://codeforces.com/contest/208/problem/A
+#
+# Author: eloyhz
+# Date: Sep/01/2020
+
+
+if __name__ == '__main__':
+    s = input()
+    song = []
+    i = 0
+    while True:
+        p = i
+        i = s.find('WUB', i)
+        if i < 0:
+            song.append(s[p:])
+            break
+        else:
+            if i != p:
+                song.append(s[p:i])
+            i += 3
+    print(' '.join(song))
