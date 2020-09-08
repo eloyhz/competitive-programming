ee1090ef5581ce5bac47883e1a635f6ca1474aeb
Eloy Hernandez <eloyhz@gmail.com>
2020-09-08 14:30:31 -0500

    SPOJ - EASYMATH [WA][Editorial]

--- spoj/easymath.py
+++ spoj/easymath.py
@@ -4,6 +4,9 @@
 # Author: eloyhz
 # Date: Sep/08/2020
 #
+# Given five numbers a1, a2, ..., a5 and a range [n, m], count ho many numbers
+# in the range are divisible by none of the five
+# Reference: https://www.quora.com/How-do-I-solve-the-problem-EASYMATH-on-SPOJ
 
 def gcd(a, b):
     if b == 0:
@@ -17,7 +20,10 @@ def lcm(a, b):
 
 
 def divisible(n, m, x):
-    return (m - n + 1) // x
+    r = (m - n + 1) // x
+    print(f'{n = }, {m = }, {x = }, {r = }')
+    return r
+
 
 def solve(n, m, a, d):
     b = [0] * 5
@@ -40,9 +46,33 @@ def solve(n, m, a, d):
     return (m - n + 1) - count
 
 
+def include_exclude(n, a):
+    ret = 0
+    for bitmask in range(1 << 5):
+        xlcm = 1
+        sign = 1
+        for i in range(5):
+            if bitmask & (1 << i):
+                sign = -sign
+                g = gcd(xlcm, a[i])
+                if n // xlcm > a[i] // g:
+                    xlcm = n + 1
+                    break
+                xlcm *= a[i] // g
+        ret += sign * (n // xlcm)
+    return ret
+
+
+def solve_editorial(n, m, a, d):
+    b = [0] * 5
+    for i in range(5):
+        b[i] = a + i * d
+    return include_exclude(m, b) - include_exclude(n - 1, b)
+
+
 if __name__ == '__main__':
     t = int(input())
     for _ in range(t):
         n, m, a, d = [int(x) for x in input().split()]
-        print(solve(n, m, a, d))
+        print(solve_editorial(n, m, a, d))
 
