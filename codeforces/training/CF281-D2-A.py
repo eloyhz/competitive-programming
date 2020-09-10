aed77caac209bb8a8cbef7fdabe12771461d27c7
Eloy Hernandez <eloyhz@gmail.com>
2020-09-10 17:22:38 -0500

    Codeforces - Word Capitalization [AC]

---
+++ codeforces/training/CF281-D2-A.py
@@ -0,0 +1,11 @@
+# Codeforces - Word Capitalization
+# https://codeforces.com/contest/281/problem/A
+#
+# Author: eloyhz
+# Date: Sep/10/2020
+#
+
+
+if __name__ == '__main__':
+    word = input()
+    print(word[0].upper() + word[1:])
