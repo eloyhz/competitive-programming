72949675e2c01b1e278be72dedb4da24cb763612
Eloy Hernandez <eloyhz@gmail.com>
2020-09-17 13:09:02 -0500

    Codeforces (CF79-D12-B) - Colorful Field [WA][Python named tuples]

--- codeforces/training/cf810_d2_b_summer_sell_off.py
+++ codeforces/training/cf810_d2_b_summer_sell_off.py
@@ -6,28 +6,26 @@
 # https://codeforces.com/contest/810/problem/B
 #
 
+from collections import namedtuple
+from operator import attrgetter
+
+Day = namedtuple('Day', ['products', 'clients'])
 
 if __name__ == '__main__':
     n, f = [int(x) for x in input().split()]
     a = []
     for _ in range(n):
         k, l = [int(x) for x in input().split()]
-        a.append([l, k])  # clients, products
-    a.sort(reverse=True)
+        a.append(Day(k, l))
+    a.sort(reverse=True, key=attrgetter('clients'))
     ans = 0
-    for x in a:
-        if x[1] == 0:   # this day there are no products
+    factor = 2
+    for d in a:
+        if d.products == 0:   # this day there are no products
             continue
         if f > 0:
-            x[1] *= 2   # double the products the first f days
             f -= 1
         else:
-            break
-    for x in a:
-        if x[1] == 0:
-            continue
-        if x[0] <= x[1]:
-            ans += x[0]
-        else:
-            ans += x[1]
+            factor = 1
+        ans += min(d.clients, d.products * factor)
     print(ans)
