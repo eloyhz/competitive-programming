9ae8f4aa1605047348ed26244f5e4ff1ecfc2736
Eloy Hernandez <eloyhz@gmail.com>
2020-09-18 17:02:16 -0500

    Codeforces (CF215-D2-B) - Olympic Medal [AC]

--- codeforces/training/cf215_d2_b_olympic_medal.py
+++ codeforces/training/cf215_d2_b_olympic_medal.py
@@ -17,12 +17,14 @@ if __name__ == '__main__':
     m = y[0]
     k = z[0]
     max_r2 = 0
+    max_p = 0
+    for i in range(m):
+        p1 = y[i + 1]
+        for j in range(k):
+            p2 = z[j + 1]
+            max_p = max(max_p, p1 / p2)
     for r in range(n):
         r1 = x[r + 1]
-        for s in range(m):
-            p1 = y[s + 1]
-            for t in range(k):
-                p2 = z[t + 1]
-                r2 = math.sqrt(p1 * r1 ** 2 / (a / b * p2 + p1))
-                max_r2 = max(r2, max_r2)
+        r2 = math.sqrt(max_p * r1 ** 2 / (a / b + max_p))
+        max_r2 = max(r2, max_r2)
     print(f'{max_r2:.12f}')
