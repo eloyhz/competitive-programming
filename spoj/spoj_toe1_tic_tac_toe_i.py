6a26ec0707d6d69221309a622335587048fe5367
Eloy Hernandez <eloyhz@gmail.com>
2020-09-22 19:13:48 -0500

    SPOJ TOE1 - Tic-Tac-Toe ( I ) [TLE][Python]

--- spoj/spoj_toe1_tic_tac_toe_i.py
+++ spoj/spoj_toe1_tic_tac_toe_i.py
@@ -5,6 +5,10 @@
 # SPOJ TOE1 - Tic-Tac-Toe ( I )
 # https://www.spoj.com/problems/TOE1/
 #
+# Python TLE
+
+from collections import deque
+from copy import deepcopy
 
 def is_valid(board):
     r = [0] * 3
@@ -44,13 +48,44 @@ def is_valid(board):
 #    print(f'{r = }, {c = }, {d = }, {count = }')
     return True if count <= 1 else False
 
+def is_win(b):
+    return b[0][0] == b[0][1] == b[0][2] and b[0][0] != '.' or \
+            b[1][0] == b[1][1] == b[1][2] and b[1][0] != '.' or \
+            b[2][0] == b[2][1] == b[2][2] and b[2][0] != '.' or \
+            b[0][0] == b[1][0] == b[2][0] and b[0][0] != '.' or \
+            b[0][1] == b[1][1] == b[2][1] and b[0][1] != '.' or \
+            b[0][2] == b[1][2] == b[2][2] and b[0][2] != '.' or \
+            b[0][0] == b[1][1] == b[2][2] and b[0][0] != '.' or \
+            b[2][0] == b[1][1] == b[0][2] and b[2]
+
+
+def solve(b):
+    q = deque()
+    u = [['.' for _ in range(3)] for _ in range(3)]
+    q.append((u, 'X'))
+    while q:
+        x = q.popleft()
+        u = x[0]
+        turn = x[1]
+        if u == b:
+            return True
+        if is_win(u):
+            continue
+        for i in range(3):
+            for j in range(3):
+                if u[i][j] == '.' and b[i][j] == turn:
+                    v = deepcopy(u)
+                    v[i][j] = turn
+                    q.append((v, 'O' if turn == 'X' else 'X'))
+    return False
+
 
 if __name__ == '__main__':
     n = int(input())
     for _ in range(n):
         board = []
         for _ in range(3):
-            board.append(input())
+            board.append(list(input()))
         input()
-        print('yes' if is_valid(board) else 'no')
+        print('yes' if solve(board) else 'no')
         
