ffcb718d3bd6c070273087fa447d25a7b0fd986a
Eloy Hernandez <eloyhz@gmail.com>
2020-10-09 14:34:19 -0500

    Codeforces (CF617-D2-B) - Chocolate [WA]

---
+++ codeforces/training/cf617_d2_b_chocolate.cpp
@@ -0,0 +1,30 @@
+//
+// Author: eloyhz
+// Date: Oct/09/2020
+//
+// Codeforces (CF617-D2-B) - Chocolate
+// https://codeforces.com/contest/617/problem/B
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    vector<int> ones;
+    for (int i = 1; i <= n; ++i)    {
+        int ai; cin >> ai;
+        if (ai) {
+            ones.push_back(i);
+        }
+    }
+    int ans = 1;
+    for (int i = 1; i < ones.size(); ++i)  {
+        ans *= ones[i] - ones[i - 1];
+    }
+    cout << ans << endl;
+    return 0;
+}
+
