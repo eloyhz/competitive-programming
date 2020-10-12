dfcb21f967a5bdaac5a011ccc3cfed7b55b2f3cb
Eloy Hernandez <eloyhz@gmail.com>
2020-10-12 13:09:23 -0500

    Codeforces (CF520-D2-B) - Two Buttons [TLE]

---
+++ codeforces/training/cf520_d2_b_two_buttons.cpp
@@ -0,0 +1,39 @@
+//
+// Author: eloyhz
+// Date: Oct/12/2020
+//
+// Codeforces (CF520-D2-B) - Two Buttons
+// https://codeforces.com/contest/520/problem/B
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+
+    cin >> n >> m;
+    if (n == m) {
+        cout << "0\n";
+    }
+    else if (n > m) {
+        cout << n - m << "\n";
+    }
+    else {
+        int count = 0;
+        while (n != m)  {
+            if (m % 2 == 0) {
+                m /= 2;
+            }
+            else    {
+                m++;
+            }
+            count++;
+        }
+        cout << count << "\n";
+    }
+    return 0;
+}
+
