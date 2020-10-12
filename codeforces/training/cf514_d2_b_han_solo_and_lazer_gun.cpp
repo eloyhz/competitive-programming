1ccb8e8169cb36cb4baa372dc22730de77bcba72
Eloy Hernandez <eloyhz@gmail.com>
2020-10-12 10:11:23 -0500

    Codeforces (CF514-D2-B) - Han Solo and Lazer Gun [WA]

---
+++ codeforces/training/cf514_d2_b_han_solo_and_lazer_gun.cpp
@@ -0,0 +1,49 @@
+//
+// Author: eloyhz
+// Date: Oct/12/2020
+//
+// Codeforces (CF514-D2-B) - Han Solo and Lazer Gun
+// https://codeforces.com/contest/514/problem/B
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long C;
+typedef complex<C> P;
+
+#define X real()
+#define Y imag()
+
+int main()
+{
+    int n, x0, y0;
+
+    cin >> n >> x0 >> y0;
+    P g = {x0, y0};
+    P p[n];
+    for (int i = 0; i < n; i++) {
+        int xi, yi; cin >> xi >> yi;
+        p[i] = P{xi, yi} - g;
+    }
+    bool d[n];
+    fill(d, d + n, false);
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (d[i]) 
+            continue;
+        d[i] = true;
+        ans++;
+        for (int j = i + 1; j < n; j++) {
+            if (d[j]) 
+                continue;
+            if ((conj(p[i]) * p[j]).Y == 0) {  // p[i] and p[j] are collinear
+                d[j] = true;
+            }
+        }
+    }
+    cout << ans << "\n";
+    return 0;
+}
+
