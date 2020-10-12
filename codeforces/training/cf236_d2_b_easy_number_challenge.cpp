f5e247b5484557fdeb123b76fe032df51b93fab4
Eloy Hernandez <eloyhz@gmail.com>
2020-10-11 21:30:31 -0500

    Codeforces (CF236-D2-B) - Easy Number Challenge [AC]

--- codeforces/training/cf236_d2_b_easy_number_challenge.cpp
+++ codeforces/training/cf236_d2_b_easy_number_challenge.cpp
@@ -1,6 +1,6 @@
 //
 // Author: eloyhz
-// Date: Oct/09/2020
+// Date: Oct/11/2020
 //
 // Codeforces (CF236-D2-B) - Easy Number Challenge
 // https://codeforces.com/contest/236/problem/B
@@ -11,10 +11,17 @@
 using namespace std;
 
 typedef long long ll;
+const int MAX = 10e6 + 1;
+
+int t[MAX];
 
 ll d(ll n)
 {
     ll total = (n == 1) ? 1 : 2;
+
+    if (t[n] != -1) {
+        return t[n];
+    }
 
     for (ll i = 2; i * i <= n; ++i) {
         if (n % i == 0) {
@@ -24,12 +31,13 @@ ll d(ll n)
             }
         }
     }
-    return total % (1 << 30);
+    return t[n] = total % (1 << 30);
 }
 
 int main()
 {
     ll a, b, c, ans = 0;
+    fill(t, t + MAX, -1);
     cin >> a >> b >> c;
     for (int i = 1; i <= a; ++i)    {
         for (int j = 1; j <= b; ++j)    {
