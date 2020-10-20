dd0017221337ca102c4df1aa470fa9f4ad15fc28
Eloy Hernandez <eloyhz@gmail.com>
2020-10-20 13:57:18 -0500

    Codeforces (CF791-D2-B) - Bear and Friendship Condition [AC w/editorial]

--- codeforces/training/cf791_d2_b_bear_and_friendship_condition.cpp
+++ codeforces/training/cf791_d2_b_bear_and_friendship_condition.cpp
@@ -5,40 +5,57 @@
 // Codeforces (CF791-D2-B) - Bear and Friendship Condition
 // https://codeforces.com/contest/791/problem/B
 //
+// Editorial: count vertices and edges in each connected component
+//            it's a clique if and only if 
+//              edges = vertices * (vertices - 1) / 2
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
+const int MAX = 150000;
+vector<int> g[MAX + 1];
+bool visited[MAX + 1];
+int n, m;
+long long edges, vertices;
+
+void explore(int v) {
+    visited[v] = true;
+    vertices++;
+    for (auto u : g[v]) {
+        edges++;
+        if (!visited[u])    {
+            explore(u);
+        }
+    }
+}
+
+bool dfs()
+{
+    for (int v = 1; v <= n; v++)    {
+        if (!visited[v])    {
+            edges = vertices = 0;
+            explore(v);
+            edges /= 2;
+            if (edges != vertices * (vertices - 1) / 2)  {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 int main()
 {
-    int n, m;
+    memset(visited, false, sizeof visited);
     cin >> n >> m;
-    set<int> g[n + 1];
     for (int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
-        g[a].insert(b);
-        g[b].insert(a);
+        g[a].push_back(b);
+        g[b].push_back(a);
     }
-    bool reasonable = true;
-    for (int x = 1; x <= n; x++)    {
-        for (auto y = g[x].begin(); y != g[x].end(); y++)   {
-            for (auto z = g[*y].begin(); z != g[*y].end(); z++) {
-                if (*z == x)    {
-                    continue;
-                }
-                if (g[*z].find(x) == g[*z].end())   {
-                    reasonable = false;
-                    break;
-                }
-            }
-        }
-        if (!reasonable)    {
-            break;
-        }
-    }
-    cout << (reasonable ? "YES" : "NO") << endl;
+    cout << (dfs() ? "YES" : "NO") << endl;
     return 0;
 }
 
