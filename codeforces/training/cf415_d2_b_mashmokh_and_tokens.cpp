87610d65a7aeee9a339e1da2af2b68675199a3c8
Eloy Hernandez <eloyhz@gmail.com>
2020-10-28 13:36:36 -0600

    Codeforces (CF415-D2-B) - Mashmokh and Tokens [AC w/editorial]

--- codeforces/training/cf415_d2_b_mashmokh_and_tokens.cpp
+++ codeforces/training/cf415_d2_b_mashmokh_and_tokens.cpp
@@ -2,7 +2,7 @@
 // Author: eloyhz
 // Date: Oct/28/2020
 //
-// Codeforces (CF415-D2-B) - Mashmokh and Tokens [WA]
+// Codeforces (CF415-D2-B) - Mashmokh and Tokens [AC w/editorial]
 // https://codeforces.com/contest/415/problem/B
 //
 
@@ -12,13 +12,14 @@ using namespace std;
 
 int main()
 {
-    int n, a, b; 
+    long long n, a, b; 
     cin >> n >> a >> b;
     for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        int ans = x - floor(x * a / (double)b) * b;
-        cout << ans << " ";
+        long long w;
+        cin >> w;
+        long long x = (w * a) / b;
+        long long k = ceil(x * b / (double)a);
+        cout << w - k << " ";
     }
     cout << endl;
 
