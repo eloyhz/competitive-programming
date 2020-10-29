9431b81e9ad7c7b623f3ba6dabe5ef2655f3a1b5
Eloy Hernandez <eloyhz@gmail.com>
2020-10-29 17:22:07 -0600

    Codeforces (CF510-D2-B) - Fox And Two Dots [AC]

--- codeforces/training/cf510_d2_b_fox_and_two_dots.cpp
+++ codeforces/training/cf510_d2_b_fox_and_two_dots.cpp
@@ -2,7 +2,7 @@
 // Author: eloyhz
 // Date: Oct/29/2020
 //
-// Codeforces (CF510-D2-B) - Fox And Two Dots
+// Codeforces (CF510-D2-B) - Fox And Two Dots [AC]
 // https://codeforces.com/contest/510/problem/B
 //
 
@@ -10,31 +10,59 @@
 
 using namespace std;
 
+char a[51][51];
+bool v[51][51];
+int start_i, start_j;
+char color;
+int n, m;
+
+bool dfs(int i, int j, int length)  {
+    length++;
+    if (length >= 4 && i == start_i && j == start_j)
+        return true;
+    if (v[i][j])    {
+        return false;
+    }
+    v[i][j] = true;
+    if (i > 0 && a[i-1][j] == color && dfs(i - 1, j, length))
+        return true;
+    if (i < n - 1 && a[i+1][j] == color && dfs(i + 1, j, length))
+        return true;
+    if (j > 0 && a[i][j-1] == color && dfs(i, j - 1, length))
+        return true;
+    if (j < m - 1 && a[i][j+1] == color && dfs(i, j + 1, length))
+        return true;
+    return false;
+}
+
+bool find_cycle(int i, int j)
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            v[i][j] = false;
+    color = a[i][j];
+    start_i = i;
+    start_j = j;
+    if (dfs(i, j, 0))
+        return true;
+    else
+        return false;
+}
+
 int main()
 {
-    int n, m;
     cin >> n >> m;
-    char a[n][m];
-    int i, j, k, l, r;
+    int i, j;
     for (i = 0; i < n; i++)
         for (j = 0; j < m; j++)
             cin >> a[i][j];
     bool cycle = false;
-    for (i = 0; i < n - 1; i++)
-        for (j = 0; j < m - 1; j++)
-            for (k = i + 1; k < n; k++)
-                for (l = j + 1; l < m; l++)   {
-                    char c = a[i][j];
-                    bool ok = true;
-                    for (r = j; r <= l && ok; r++)
-                        if (a[i][r] != c || a[k][r] != c)
-                            ok = false;
-                    for (r = i; r <= k && ok; r++)
-                        if (a[r][j] != c || a[r][l] != c)
-                            ok = false;
-                    if (ok)
-                        cycle = true;
-                }
+    for (i = 0; i < n && !cycle; i++)
+        for (j = 0; j < m; j++)
+            if (find_cycle(i, j))  {
+                cycle = true;
+                break;
+            }
     cout << (cycle ? "Yes" : "No") << endl;
 
     return 0;
