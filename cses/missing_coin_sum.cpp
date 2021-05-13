76a1d020c309dcc6a89b7bcd0a94c7395e138c04
Eloy Hernandez <eloyhz@gmail.com>
2021-05-13 11:57:38 -0500

    CSES - Missing Coin Sum

---
+++ cses/missing_coin_sum.cpp
@@ -0,0 +1,33 @@
+/*
+
+CSES - Stick Lengths
+https://cses.fi/problemset/task/1074
+Date: 13/05/21
+
+Reference: https://www.youtube.com/watch?v=l6rjY30bwI8
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()  {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> x(n);
+    for (int i = 0; i < n; i++) 
+        cin >> x[i];
+    sort(x.begin(), x.end());
+    long long k = 0;
+    int i = 0;
+    while (i < n && k + 1 - x[i] >= 0)   {
+        k += x[i];
+        i++;
+    }
+    cout << k + 1 << "\n";
+    return 0;
+}
