3c7ab7d7eba5ec626851a19462d87334fd046942
Eloy Hernandez <eloyhz@gmail.com>
2021-05-13 09:30:13 -0500

    CSES - Maximum Subarray Sum (Kadane's Algorithm) update

--- cses/maximum_subarray_sum.cpp
+++ cses/maximum_subarray_sum.cpp
@@ -10,10 +10,8 @@ Date: 12/05/21
 
 using namespace std;
 
-int main()  {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
+// Solution from Competitive Programmer's Handbook (Laaksonen)
+void solve()   {
     int n;
     cin >> n;
     vector<int> x(n);
@@ -26,5 +24,29 @@ int main()  {
         ans = max(ans, sum);
     }
     cout << ans << "\n";
+}
+
+// Solution from Competitive Programming 4 (Halim & Halim)
+void solve_reset_0()    {
+    int n;
+    cin >> n;
+    long long ans = 0;
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        sum += x;
+        ans = max(ans, sum);
+        if (sum < 0)
+            sum = 0;
+    }
+    cout << ans << "\n";
+}
+
+int main()  {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
     return 0;
 }
