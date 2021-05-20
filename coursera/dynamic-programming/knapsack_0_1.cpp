aa65af04e25c94184dff8762e4a1e0074b2a8310
Eloy Hernandez <eloyhz@gmail.com>
2021-05-20 10:40:47 -0500

    Coursera - Dynamic Programming - Knapsack 0-1 (refactoring)

--- coursera/dynamic-programming/knapsack_0_1.cpp
+++ coursera/dynamic-programming/knapsack_0_1.cpp
@@ -8,29 +8,22 @@ int main()  {
     int s;  // weight limit
     int n;  // amount of bars
     cin >> s >> n;
-    vector<int> a(n + 1);
+    vector<int> w(n + 1);
     for (int i = 1; i <= n; i++)
-        cin >> a[i];
+        cin >> w[i];
     vector<vector<bool>> dp(n + 1, vector<bool>(s + 1, false));
     dp[0][0] = true;
-    for (int i = 1; i <= n; i++)
-        for (int w = 0; w <= s; w++)    {
-            dp[i][w] = dp[i - 1][w];
-            if (w - a[i] >= 0)  {
-                cout << "i = " << i << "\n";
-                dp[i][w] = dp[i][w] || dp[i - 1][w - a[i]];
+    for (int i = 1; i <= n; i++)        // rows = bars (items)
+        for (int j = 0; j <= s; j++)    {   // columns = weights
+            dp[i][j] = dp[i - 1][j];
+            if (j - w[i] >= 0)  {
+                dp[i][j] = dp[i][j] || dp[i - 1][j - w[i]];
             }
         }
-    cout << "\n";
-    for (int i = 0; i <= n; ++i)    {
-        for (int j = 0; j <= s; ++j) 
-            cout << dp[i][j] << " ";
-        cout << "\n";
-    }
     int ans = 0;
-    for (int w = s; w >= 0; --w)
-        if (dp[n][w])   {
-            ans = w;
+    for (int j = s; j >= 0; --j)
+        if (dp[n][j])   {
+            ans = j;
             break;
         }
     cout << ans << "\n";
