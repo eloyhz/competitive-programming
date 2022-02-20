4c7ad4e2e39c7bccfd44fd6f6d505545db9aef7b
EloyHz <eloyhz@gmail.com>
2022-02-19 22:26:55 -0600

    Acepta el reto - Suma descendente

---
+++ acepta-el-reto/265_suma_descendente.c
@@ -0,0 +1,27 @@
+/**
+ * Acepta el reto - Suma descendente
+ * 19-02-2022
+ */
+
+#include <stdio.h>
+
+int main(void)  {
+    int n;
+    while(1) {
+        scanf("%d", &n);
+        if (n == 0) {
+            break;
+        }
+        int num = 0;
+        int sum = 0;
+        int d = 1;
+        while (n > 0)   {
+            num += n%10*d;
+            n /= 10;
+            d *= 10;
+            sum += num;
+        }
+        printf("%d\n", sum);
+    }
+    return 0;
+}
